68c6e3a18059c01f98f672c06cb295d1a3b25753
Matic Babnik <matic.babnik@outlook.com>
2025-10-13 02:38:44 +0200

    BIG numbers (#1)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
 
 const app = new Elysia()
-    .get("/randomNumber", () => ({ result: ~~(Math.random() * 1000) }))
+    .get("/randomNumber", () => ({ result: ~~(Math.random() * 10000) }))
     .get('/health', () => 'OK')
     .listen(3000);
 
